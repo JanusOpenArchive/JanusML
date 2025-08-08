adbdf66a43bfe7fd22d1de6ac212bd76c60f9d7f
thejupitergroup <ourjupitergroup@gmail.com>
2025-08-08 07:24:54 -0400

    Alpha 3.3

--- encoder/script.js
+++ encoder/script.js
@@ -98,6 +98,15 @@ const md = window.janusml()
             }
         }
     })
+    .use(janusmlContainer, 'error', {
+        render: function (tokens, idx) {
+            if (tokens[idx].nesting === 1) {
+                return '<div class="container-box error">';
+            } else {
+                return '</div>\n';
+            }
+        }
+    })
     .use(janusmlContainer, 'side', {
         render: function (tokens, idx) {
             if (tokens[idx].nesting === 1) {
